177a876b0834ee6d7782c3cebad6833ee0ec4d25
Teacher <itstep.codeclub@gmail.com>
2023-02-26 20:16:42 +0200

    Добавьте файлы проекта.

---
+++ MyCompany/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCompany.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DepName { get; set; }
+
+        public virtual List<Employee> Employees { get; set; }
+    }
+}
